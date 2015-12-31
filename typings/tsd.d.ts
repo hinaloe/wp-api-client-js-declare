89ae4a1b78504e821a3ac6e3f9b52eef4156c780
hina <hina@hinaloe.net>
2015-12-31 10:28:50 +0900

    init

---
+++ typings/tsd.d.ts
@@ -0,0 +1,7 @@
+
+/// <reference path="backbone/backbone-global.d.ts" />
+/// <reference path="backbone/backbone.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="underscore/underscore.d.ts" />
+
+/// <reference path="wp-api/index.d.ts" />
